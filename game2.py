85dae8ca9cae74f0038e49878c920d1cf2779d6b
Roman Bolotaev <8485558@gmail.com>
2024-04-18 23:14:38 +0400

    before graphics

--- game2.py
+++ game2.py
@@ -1,7 +1,63 @@
-from parts2 import Board
+from gameparts2 import Board
 
-game = Board()
-game.display()
-game.make_move(1, 1, 'X')
-print('Ход сделан')
-game.display()
+from gameparts2.exceptions2 import FieldIndexError, CellOccupiedError
+
+
+def main():
+    game = Board()
+    current_player = 'X'
+    running = True
+    game.display()
+
+    while running:
+        print(f'Ход делает {current_player}')
+
+        while True:
+            try:
+                row = int(input('Введите номер строки: '))
+                if row < 0 or row >= game.field_size:
+                    raise FieldIndexError
+
+                col = int(input('Введите номер столбца: '))
+                if col < 0 or col >= game.field_size:
+                    raise FieldIndexError
+                
+                if game.board[row][col] != ' ':
+                    raise CellOccupiedError
+
+            except FieldIndexError:
+                print(
+                    'Значение должно быть неотрицательным и меньше '
+                    f'{Board.field_size}'
+                )
+                print('Пожалуйста, введите значения заново')
+                continue
+            except ValueError:
+                print('Буквы вводить нельзя. Только числа.')
+                print('Пожалуйста, введите значения для строки и столбца заново.')
+                continue
+            except CellOccupiedError:
+                print('Ячейка занята')
+                print('Введите другие координаты')
+                continue
+            except Exception as e:
+                print(f'Возникла ошибка {e}')
+                continue
+            else:
+                break
+
+        game.make_move(row, col, current_player)
+        game.display()
+
+        if game.check_win(current_player):
+            print(f'Победил {current_player}')
+            running = False
+        elif game.is_board_full():
+            print('Ничья')
+            running = False
+        
+        current_player = 'O' if current_player == 'X' else 'X'
+
+
+if __name__ == '__main__':
+    main()
